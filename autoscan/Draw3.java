586ebe00070a632b7c3944c2bc12a6018eaaa519
Eugene E. Kwan <ekwan16@gmail.com>
2017-05-07 13:51:04 -0400

    snar

--- autoscan/Draw3.java
+++ autoscan/Draw3.java
@@ -10,27 +10,29 @@ public class Draw3
         {
             // read input file
 	    
-            GJFfile input_gjf = new GJFfile("benzyl_bromide_F_1H2O_Me4N_ts.gjf");
+            GJFfile input_gjf = new GJFfile("pyridine_F_N3_template.gjf");
 
             Molecule molecule = input_gjf.molecule;
 
             // define the atom numbers to be frozen
             // the fragment attached to "toAtomNumber" will be moved
-            int fromAtomNumber1 = 12;
-            int toAtomNumber1   = 16;
-            int fromAtomNumber2 = 12;
-            int toAtomNumber2   = 15;
+            int fromAtomNumber1 = 9;
+            int toAtomNumber1   = 10;
+            int fromAtomNumber2 = 9;
+            int toAtomNumber2   = 11;
 
             molecule = molecule.addBond(fromAtomNumber1, toAtomNumber1);
             molecule = molecule.addBond(fromAtomNumber2, toAtomNumber2);
+            molecule = molecule.addBond(11,12);
+            molecule = molecule.addBond(12,13);
 
             // define the keywords for the input files that will be generated
-            String keywords = "%mem=3GB\n%nprocshared=4\n#p geom=connect b3lyp/6-31g(d) empiricaldispersion=gd3bj scrf=(pcm,solvent=THF) pop=none opt=modredundant freq=noraman";
+            String keywords = "%mem=3GB\n%nprocshared=4\n#p b3lyp 6-31+g* empiricaldispersion=gd3bj pop=none opt=modredundant freq=noraman";
             String tail = String.format("B %d %d F\nB %d %d F\n\n", fromAtomNumber1, toAtomNumber1, fromAtomNumber2, toAtomNumber2);
             
-            for (double distance1 = 2.0; distance1 <= 2.6; distance1 += 0.04)
+            for (double distance1 = 1.35; distance1 <= 2.05; distance1 += 0.05)
                 {
-                    for ( double distance2 = 1.95; distance2 <= 2.40; distance2 += 0.04)
+                    for ( double distance2 = 1.45; distance2 <= 2.05; distance2 += 0.05)
                         {
                             Molecule newMolecule = molecule.setDistance(fromAtomNumber1, toAtomNumber1, distance1);
                             newMolecule = newMolecule.setDistance(fromAtomNumber2, toAtomNumber2, distance2);
@@ -41,8 +43,10 @@ public class Draw3
                             Atom atom3 = newMolecule.contents.get(fromAtomNumber2-1);
                             Atom atom4 = newMolecule.contents.get(toAtomNumber2-1);
 
-                            String filename = String.format("gjf/benzyl_bromide_F_1H2O_Me4N_autoscan_%03.0f_%03.0f.gjf", distance1 * 100, distance2 * 100);
-                            GaussianInputFile output_gjf = new GaussianInputFile(newMolecule, "title", keywords, tail);
+                            String filename = String.format("gjf/pyridine_F_N3-%03.0f_%03.0f-b3lyp_d3bj-631+gd-gas.gjf", distance1 * 100, distance2 * 100);
+                            
+                            // molecule, name, keywords, tail, writeConnectivity, charge, multiplicity
+                            GaussianInputFile output_gjf = new GaussianInputFile(newMolecule, "title", keywords, tail, false, -1, 1);
                             output_gjf.write(filename);
                             
                             System.out.printf("%.2f\t%.2f\t%s\n", newMolecule.getDistance(atom1, atom2), newMolecule.getDistance(atom3, atom4), filename);
